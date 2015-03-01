9783eb1450dd8e92425b561cdb9a295da0f6312e
unknown <alonfai@gmail.com>
2015-03-02 01:27:23 +1100

    optimizing code

--- 02-pack-it/index.js
+++ 02-pack-it/index.js
@@ -48,14 +48,14 @@
         for (var index = 0 ; index<buffer.length; ++index) {
             var numBitwise = buffer[index].charCodeAt().toString(2);
 
-            if (numBitwise.length < maxCharBitSize) { //complete to 8 bit string representation with leading zeros on the left
-                for (var len = numBitwise.length ; len < maxCharBitSize ; ++len) {
-                    numBitwise = '0' + numBitwise;
-                }
+            for (var charCounter = numBitwise.length - 1 ;  charCounter >= 0 ; --charCounter) { //write bits to resulting array
+                res.push(parseInt(numBitwise[charCounter], 10) === 1);
             }
 
-            for (var charCounter = maxCharBitSize-1 ;  charCounter >= 0 ; --charCounter) { //write bits to resulting array
-                res.push(parseInt(numBitwise[charCounter], 10) === 1);
+            if (numBitwise.length < maxCharBitSize) { //complete to 8 bit array representation with appending "false" flags at the resulting array
+                for (var len = numBitwise.length ; len < maxCharBitSize ; ++len) {
+                    res.push(false);
+                }
             }
         }
 
